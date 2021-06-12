085296e12bb8868b2fecabd49f19513879175cf4
budds300 <budds300@gmail.com>
2021-06-12 06:33:47 +0300

    updated model. addition of pitches functions and class

--- app/models.py
+++ app/models.py
@@ -37,7 +37,55 @@ class User(UserMixin,db.Model):
     def __repr__(self):
         return f'User {self.username}'
 
-
+class Category:
+    '''
+    Defines different cartegories of pitches
+    '''
+    __tablename__ = pitchcategories
+    
+    id = db.Column(db.Integer,primary_key=True)
+    name_of_category = db.Column(db.String(225))
+    description = db.Column(db.String(225))
+    
+    @classmethod
+    def get_categies(cls):
+        '''
+        This function fetches all the categories from the database
+        '''
+        categories = Category.query.all()
+        return categories
+    
+class Pitch():
+    
+    __tablename__ = 'pitch'
+    
+    id = db.Column(db.Integer,primary_key=True)
+    pitch = db.Colum(db.String(255))
+    category_id = db.Column(db.Integer)
+    description = db.Column(db.String(225))
+    date = db.Column(db.DateTime, default= datetime.utcnow())
+    comments = db.relationship('Comments',backref = 'users',lazy = 'dynamic')
+    downvotes = db.relationship('Downvote',backref = 'users',lazy = 'dynamic')
+    upvotes = db.relationship('Upvote',backref = 'users',lazy = 'dynamic')
     
     
+    def save_pitch(self):
+        '''
+        function that saves the pictch
+        '''
+        db.session.add(self)
+        db.session.commit
     
+    @classmethod
+    def get_all_pitches(self):
+        ''' 
+        Function retrieves queriesthe data returns based on cartegory
+        '''
+        return Pitch.query.all()
+    
+    @classmethod
+    def get_all_pitches_by_category(cls,category_id):
+        ''' 
+        Function retrieves queriesthe data returns based on cartegory
+        '''
+        return Pitch.query.filter_by(category_id = category_id)
